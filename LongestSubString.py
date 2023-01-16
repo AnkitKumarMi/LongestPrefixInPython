3fc69d6c4b1c844b93bb126c38a0a8731471d8ed
Ankit Kumar Mishra <122213779+AnkitKumarMi@users.noreply.github.com>
2023-01-17 01:00:36 +0530

    Update LongestSubString.py

--- LongestSubString.py
+++ LongestSubString.py
@@ -1,37 +1,7 @@
-n=int(input())
-s=input().split()
-rs=''
-if n<2:
-    print(s[0])
-s.sort()
-chk=s[0]
-print(chk)
-s.remove(s[0])
-print(s)
-for i in range(len(chk)):
-    flag=0
-    for j in s:
-        if chk[i]==j[i]:
-
-            flag+=1
-        else:
-            break
-    if flag==len(s):
-            rs+=chk[i]
-    else:
-            break
-if len(rs)==0:
-        print(-1)
-else:
-        print(rs)
-
-
-
-
-
-
-
-
-
-
-
+s2=input()
+rs1=''
+s3=sorted(s2)
+for i in s3:
+    if len(i)>len(rs1):
+        rs1=i
+print(rs1)
